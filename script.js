316825527a8af990b6c5d4e51670112505af3cbf
Rishi <rishikantchoudhary951@gmail.com>
2023-08-25 19:52:12 +0530

    Added feature to add note on save, but delete note still has some bugs

--- script.js
+++ script.js
@@ -6,12 +6,6 @@ Getting the elements.
 
 const displayNote = document.querySelector('.display-note');
 
-const notes = document.querySelectorAll('.note');
-const deleteNoteButtons = document.querySelectorAll('.delete-note');
-
-const noteTitles = document.querySelectorAll('.note-title');
-const noteContents = document.querySelectorAll('.note-content');
-
 /*
 
 Creating the functions.
@@ -39,32 +33,19 @@ Hooking the functions with the elements.
 window.addEventListener('resize', myfunc);
 myfunc()
 
-// to add "..." if content overflows
-noteTitles.forEach((noteTitle) => {
-    if (noteTitle.scrollHeight > noteTitle.offsetHeight) {
-        noteTitle.innerText = `${noteTitle.innerText.slice(0, 35)}...`;
-    }
-});
-noteContents.forEach((noteContent)=>{
-    if (noteContent.scrollHeight > noteContent.offsetHeight) {
-        noteContent.innerText = `${noteContent.innerText.slice(0, 105)}...`;
-    }
-});
 
 
-
-const openAddNoteSection = ()=>{
+const openAddNoteSection = () => {
     addNoteButton.classList.toggle('cancel-button');
     addNote.classList.toggle('add-note-active');
     saveNoteButton.classList.toggle('save-note-button-active');
     plusInAddNoteButton.forEach(plus => plus.classList.toggle('toggle-cross'));
-
+    
     noteTitleText.value = '';
     noteContentText.value = '';
 }
 
 
-const addNoteText = document.querySelector('.add-note-text');
 const addNote = document.querySelector('.add-note');
 const addNoteButton = document.querySelector('.add-note-button');
 const saveNoteButton = document.querySelector('.save-note-button');
@@ -78,32 +59,58 @@ addNoteButton.addEventListener('click', openAddNoteSection)
 
 // save button;
 
-const saveNoteInLocalStorage = ()=>{
-    if(noteTitleText.value === '') return;  // retutns the function if title is empty.
+const singleDigitToDoubleDigit = (input) => {
+    if (input < 10) {
+        input = `0${input}`;
+    }
+    return input;
+}
 
+const saveNoteInLocalStorage = () => {
+    if (noteTitleText.value === '') return;  // retutns the function if title is empty.
+    
     const date = new Date;
-    const [currentYear, currentMonth, currentDate, currentHour, currentMinutes] = [date.getFullYear(), date.getMonth() + 1, date.getDate(), date.getHours(), date.getMinutes()];
-
-    const dateAndTime = `${currentDate}-${currentMonth}-${currentYear} ${currentHour}:${currentMinutes}`
-
-    localStorage.setItem(noteTitleText.value, noteContentText.value);
-
-    if(noteTitleText.value.length > 36){
-        noteTitleText.value = `${noteTitleText.value.slice(0,35)}...`;
-    }
-    if(noteContentText.value.length > 106){
-        noteContentText.value = `${noteContentText.value.slice(0,105)}...`;
-    }
-
-    displayNote.innerHTML = displayNote.innerHTML + `<div class="note"><button class="delete-note"><img src="./assets/images/note-delete-cross.png" alt="Delete" class="delete-note-png"></button><div class="note-container"><p class="note-title">${noteTitleText.value}</p><hr class="note-hr"><p class="note-content">${noteContentText.value}</p></div><p class="note-dateAndTime">${dateAndTime}</p></div>`
-
+    const currentYear = date.getFullYear();
+    const currentMonth = singleDigitToDoubleDigit(date.getMonth() + 1);
+    const currentDate = singleDigitToDoubleDigit(date.getDate());
+    const currentHour = singleDigitToDoubleDigit(date.getHours());
+    const currentMinutes = singleDigitToDoubleDigit(date.getMinutes());
+    
+    const dateAndTime = `${currentDate}-${currentMonth}-${currentYear} ${currentHour}:${currentMinutes}`;
+    
+    const noteContentValueAndDateTime = `${noteContentText.value} -+- ${dateAndTime}`;
+    
+    localStorage.setItem(noteTitleText.value, noteContentValueAndDateTime);
+    
+    displayNote.innerHTML = displayNote.innerHTML + `<div class="note"><button class="delete-note"><img src="./assets/images/note-delete-cross.png" alt="Delete" class="delete-note-png"></button><div class="note-container"><p class="note-title">${noteTitleText.value}</p><hr class="note-hr"><p class="note-content scrollbar">${noteContentText.value}</p></div><p class="note-dateAndTime">${dateAndTime}</p></div>`
+    
     noteTitleText.value = '';
     noteContentText.value = '';
 }
 
-saveNoteButton.addEventListener('click',saveNoteInLocalStorage);
+saveNoteButton.addEventListener('click', saveNoteInLocalStorage);
 
 
-// Object.keys(localStorage).forEach((key)=>{
-//     console.log(localStorage.getItem(l=key))
-// })
+const listNotesInStorageButton = () => {
+    displayNote.innerHTML = ``;
+    Object.keys(localStorage).forEach((key) => {
+        const value = localStorage.getItem(key);
+        const indexOfSeparatitionCode = value.indexOf("-+-");
+        const noteContentInLocalStorage = value.slice(0, indexOfSeparatitionCode - 2);
+        const dateTimeInLocalStorage = value.slice(indexOfSeparatitionCode + 3);
+        
+        displayNote.innerHTML = displayNote.innerHTML + `<div class="note"><button class="delete-note"><img src="./assets/images/note-delete-cross.png" alt="Delete" class="delete-note-png"></button><div class="note-container"><p class="note-title">${key}</p><hr class="note-hr"><p class="note-content scrollbar">${noteContentInLocalStorage}</p></div><p class="note-dateAndTime">${dateTimeInLocalStorage}</p></div>`;
+    })
+};
+listNotesInStorageButton();
+
+const deleteNoteButtons = document.querySelectorAll('.delete-note');
+
+const deleteSelectedNote = function(){
+    console.log('hello');
+    const selectedKey = this.nextSibling.firstElementChild.innerText;
+    localStorage.removeItem(selectedKey);
+    listNotesInStorageButton()
+}
+
+deleteNoteButtons.forEach(button => button.addEventListener('click', deleteSelectedNote));
