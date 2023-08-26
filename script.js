4665a80248dba953d180f8778dd76f6d9a99aaf3
Rishi <rishikantchoudhary951@gmail.com>
2023-08-26 12:41:52 +0530

    Fixed the delete note bug.

--- script.js
+++ script.js
@@ -1,18 +1,7 @@
-/*
-
-Getting the elements.
-
-*/
+//++++++++++++++++++++++ To set number of columns as per screen width. ++++++++++++++++++++++++++++++++++++++++++++
 
 const displayNote = document.querySelector('.display-note');
 
-/*
-
-Creating the functions.
-
-*/
-
-// to set number of columns as per screen width.
 const myfunc = () => {
     const displayNoteWidth = displayNote.offsetWidth;
     let numberOfNotesInARow = Math.floor(displayNoteWidth / 198);
@@ -24,16 +13,17 @@ const myfunc = () => {
     displayNote.style.gridTemplateColumns = `repeat(${numberOfNotesInARow}, 1fr)`;
 }
 
-/*
-
-Hooking the functions with the elements.
-
-*/
-
 window.addEventListener('resize', myfunc);
 myfunc()
 
 
+//++++++++++++++++++++++ To apply transitions and classes when clicked on add note button. ++++++++++++++++++++++++
+
+const addNoteButton = document.querySelector('.add-note-button');
+
+const addNote = document.querySelector('.add-note');
+const saveNoteButton = document.querySelector('.save-note-button');
+const plusInAddNoteButton = document.querySelectorAll('.plus');
 
 const openAddNoteSection = () => {
     addNoteButton.classList.toggle('cancel-button');
@@ -41,24 +31,20 @@ const openAddNoteSection = () => {
     saveNoteButton.classList.toggle('save-note-button-active');
     plusInAddNoteButton.forEach(plus => plus.classList.toggle('toggle-cross'));
     
+    // to empty the input text fields when clicked on cancel button(which is also add note button).
     noteTitleText.value = '';
     noteContentText.value = '';
 }
 
-
-const addNote = document.querySelector('.add-note');
-const addNoteButton = document.querySelector('.add-note-button');
-const saveNoteButton = document.querySelector('.save-note-button');
-const plusInAddNoteButton = document.querySelectorAll('.plus');
-const noteTitleText = document.querySelector('.title-text');
-const noteContentText = document.querySelector('.body-text');
-
 addNoteButton.addEventListener('click', openAddNoteSection)
 
 
+//++++++++++++++++++++++ To save not after filling the input text fields. ++++++++++++++++++++++++++++++++++++++++++
 
-// save button;
+const noteTitleText = document.querySelector('.title-text');
+const noteContentText = document.querySelector('.body-text');
 
+// To format the digits of month date hour and minutes;
 const singleDigitToDoubleDigit = (input) => {
     if (input < 10) {
         input = `0${input}`;
@@ -67,8 +53,10 @@ const singleDigitToDoubleDigit = (input) => {
 }
 
 const saveNoteInLocalStorage = () => {
-    if (noteTitleText.value === '') return;  // retutns the function if title is empty.
+    // retutns the function if title is empty.
+    if (noteTitleText.value === '') return;
     
+    // date and time of saved note.
     const date = new Date;
     const currentYear = date.getFullYear();
     const currentMonth = singleDigitToDoubleDigit(date.getMonth() + 1);
@@ -78,12 +66,19 @@ const saveNoteInLocalStorage = () => {
     
     const dateAndTime = `${currentDate}-${currentMonth}-${currentYear} ${currentHour}:${currentMinutes}`;
     
+    // to store as value in local storage note content and date time of note is fused in one string.
     const noteContentValueAndDateTime = `${noteContentText.value} -+- ${dateAndTime}`;
     
     localStorage.setItem(noteTitleText.value, noteContentValueAndDateTime);
     
+    // for immediate display of note when note is saved.
     displayNote.innerHTML = displayNote.innerHTML + `<div class="note"><button class="delete-note"><img src="./assets/images/note-delete-cross.png" alt="Delete" class="delete-note-png"></button><div class="note-container"><p class="note-title">${noteTitleText.value}</p><hr class="note-hr"><p class="note-content scrollbar">${noteContentText.value}</p></div><p class="note-dateAndTime">${dateAndTime}</p></div>`
     
+    // for hooking delete note feature immediatly after note is saved(if not we need to refresh the page.).
+    deleteNoteButtons = document.querySelectorAll('.delete-note');
+    deleteNoteButtons.forEach(button => button.addEventListener('click', deleteSelectedNote));
+    
+    // to empty the input text fields when clicked on cancel button(which is also add note button).
     noteTitleText.value = '';
     noteContentText.value = '';
 }
@@ -91,10 +86,16 @@ const saveNoteInLocalStorage = () => {
 saveNoteButton.addEventListener('click', saveNoteInLocalStorage);
 
 
-const listNotesInStorageButton = () => {
+//++++++++++++++++++++++ To list all the saved note in localStorage ++++++++++++++++++++++++++++++++++++++++++++++++
+
+const listNotesInStorage = () => {
+    // to empty the display before area before listing all the notes( in case of refresh ).
     displayNote.innerHTML = ``;
+    
     Object.keys(localStorage).forEach((key) => {
         const value = localStorage.getItem(key);
+        
+        // to separate note content and date and time of saved note.
         const indexOfSeparatitionCode = value.indexOf("-+-");
         const noteContentInLocalStorage = value.slice(0, indexOfSeparatitionCode - 2);
         const dateTimeInLocalStorage = value.slice(indexOfSeparatitionCode + 3);
@@ -102,15 +103,24 @@ const listNotesInStorageButton = () => {
         displayNote.innerHTML = displayNote.innerHTML + `<div class="note"><button class="delete-note"><img src="./assets/images/note-delete-cross.png" alt="Delete" class="delete-note-png"></button><div class="note-container"><p class="note-title">${key}</p><hr class="note-hr"><p class="note-content scrollbar">${noteContentInLocalStorage}</p></div><p class="note-dateAndTime">${dateTimeInLocalStorage}</p></div>`;
     })
 };
-listNotesInStorageButton();
+listNotesInStorage();
 
-const deleteNoteButtons = document.querySelectorAll('.delete-note');
+
+//++++++++++++++++++++++ To delete the hovered note from localStorage and display area. +++++++++++++++++++++++++++++
+
+let deleteNoteButtons = document.querySelectorAll('.delete-note');
 
 const deleteSelectedNote = function(){
-    console.log('hello');
+    // to get the title of the note as it is the key.
     const selectedKey = this.nextSibling.firstElementChild.innerText;
     localStorage.removeItem(selectedKey);
-    listNotesInStorageButton()
+
+    // to list all other notes after deleting one note.
+    listNotesInStorage()
+
+    // to reassign all the delete buttons after listing all notes again.
+    deleteNoteButtons = document.querySelectorAll('.delete-note');
+    deleteNoteButtons.forEach(button => button.addEventListener('click', deleteSelectedNote));
 }
 
 deleteNoteButtons.forEach(button => button.addEventListener('click', deleteSelectedNote));
